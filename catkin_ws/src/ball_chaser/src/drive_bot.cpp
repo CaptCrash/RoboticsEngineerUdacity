6401aca75bcb0e989f2e728534433d9c7d0347b9
Ben Remer <remerben@yahoo.com>
2021-05-27 17:16:54 -0700

    Ball chaser project

---
+++ catkin_ws/src/ball_chaser/src/drive_bot.cpp
@@ -0,0 +1,42 @@
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include "ball_chaser/DriveToTarget.h"
+
+// ROS::Publisher motor commands;
+ros::Publisher motor_command_publisher;
+
+// TODO: Create a handle_drive_request callback function that executes whenever a drive_bot service is requested
+// This function should publish the requested linear x and angular velocities to the robot wheel joints
+// After publishing the requested velocities, a message feedback should be returned with the requested wheel velocities
+bool handle_drive_request(ball_chaser::DriveToTarget::Request& req, ball_chaser::DriveToTarget::Response& res){
+    // Create a motor_command object of type geometry_msgs::Twist
+    geometry_msgs::Twist motor_command;
+    motor_command.linear.x = req.angular_z; //flipping these until I figure out whats wrong with the controller
+    motor_command.angular.z = -req.linear_x;
+    // Publish angles to drive the robot
+    motor_command_publisher.publish(motor_command);
+
+    res.msg_feedback = "Command sent with velocity " + std::to_string(motor_command.linear.x) + "and rotation " + std::to_string(motor_command.angular.z);
+    ROS_INFO_STREAM(res.msg_feedback);
+    return(true);
+}
+
+
+int main(int argc, char** argv)
+{
+    // Initialize a ROS node
+    ros::init(argc, argv, "drive_bot");
+
+    // Create a ROS NodeHandle object
+    ros::NodeHandle n;
+
+    // Inform ROS master that we will be publishing a message of type geometry_msgs::Twist on the robot actuation topic with a publishing queue size of 10
+    motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+
+    // Initial service to send velocity commands
+    ros::ServiceServer service = n.advertiseService("drive_bot/handle_drive_request", handle_drive_request);
+    // TODO: Handle ROS communication events
+    ros::spin();
+
+    return 0;
+}
---
+++ catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -0,0 +1,78 @@
+#include "ros/ros.h"
+#include "ball_chaser/DriveToTarget.h"
+#include <sensor_msgs/Image.h>
+
+// Define a global client that can request services
+ros::ServiceClient client;
+ball_chaser::DriveToTarget srv;
+float MAXANGULARVEL = 5;
+float MAXVELOCITY = 5;
+float STOPDISTANCE = .1;
+// This function calls the command_robot service to drive the robot in the specified direction
+void drive_robot(float lin_x, float ang_z)
+{
+    srv.request.linear_x = lin_x;
+    srv.request.angular_z = ang_z;
+    if (client.call(srv)){
+        //log success
+        ROS_INFO("Call Success!");
+    }else{
+        //log failure
+        ROS_WARN("Call Failed!");
+    }
+}
+
+// This callback function continuously executes and reads the image data
+void process_image_callback(const sensor_msgs::Image img)
+{
+
+    int white_pixel = 255; //255 would be pure white but camera seems to be giving lower values
+    int height = img.height;
+    int width = img.width;
+    bool ballFound = false;
+    int pixelsFound = 0;
+    float posX = 0;
+    float posY = 0;
+    for (int pixel = 0; pixel<height*width;pixel++){
+        if (img.data[pixel]>=white_pixel){
+            ballFound = true;
+            int posX = pixel%width;
+            break;
+//            int Y = pixel/width;
+            // Keep updating the ball position as we find pixels. Note that we are assuming every white pixel is part of the ball.
+//            posX = (posX*pixelsFound + X) / (pixelsFound+1);
+//            posY = (posY*pixelsFound + Y) / (pixelsFound+1);
+//            pixelsFound = pixelsFound + 1;
+        }
+    }
+    // By default we stop (default is no ball found)
+    float lin_x = 0;
+    float ang_z = 0;
+    if (ballFound){ //P controller
+        ROS_INFO("Ball Found, Calculating Request");
+        //Ball detected, we need to calculate a velocity and rotation so that we can follow the ball
+        lin_x = MAXVELOCITY;// / float(pixelsFound) / (float(height)*float(width)); // We want to go faster it the ball is further away
+        float center = width / 2;
+        ang_z = MAXANGULARVEL * (center - posX) / width; //left turn is positive, posX < center. We turn sharper when off by morel
+    }
+    
+    drive_robot(lin_x,ang_z);
+}
+
+int main(int argc, char** argv)
+{
+    // Initialize the process_image node and create a handle to it
+    ros::init(argc, argv, "process_image");
+    ros::NodeHandle n;
+
+    // Define a client service capable of requesting services from command_robot
+    client = n.serviceClient<ball_chaser::DriveToTarget>("/drive_bot/handle_drive_request");
+
+    // Subscribe to /camera/rgb/image_raw topic to read the image data inside the process_image_callback function
+    ros::Subscriber sub1 = n.subscribe("/camera/rgb/image_raw", 1, process_image_callback);
+
+    // Handle ROS communication events
+    ros::spin();
+
+    return 0;
+}
